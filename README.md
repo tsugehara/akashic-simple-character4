fe49292c100cbe66ae4577443d2b8e123ec7476b
tsugehara <tsugehara@gmail.com>
2021-10-14 15:03:00 +0900

    implement

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
-# akashic-module-template
+# akashic-simple-character4
 
-**akashic-module-template**は [Akashic Engine](https://akashic-games.github.io/)のモジュールをなる早で作るためのテンプレートリポジトリです。
+**akashic-simple-character4**は [Akashic Engine](https://akashic-games.github.io/)のためのシンプルなキャラクターエンティティです。
+
+4方向のキャラセットに対応しています。
+
+足踏みしたり、方向転換をしたりします。
 
 ## 利用方法
 
-templateを使ってrepositoryを作成した後は、忘れずに`package.json`のnameとREADME.mdの記述内容を変更し、 `npm i` をしてから開発をしてください。
+`akashic install akashic-simple-character4` で利用してください。
 
-`src/index.ts` に全部のソースコードを書いてください。
+`g.FrameSprite` と使い勝手は大体同じですが、以下の拡張があります。
 
-もしソースコードを増やす場合は、 `package.json` の `files` を直すのを忘れないようにしてください。
+1. 初期値にanimationFrameCountがある
+	- キャラクターが足踏みするフレーム数を指定してください
+2. animationMoveTo, animationMoveByがある
+	- キャラクターを一定時間かけて移動させます
+3. moveがある
+	- 方向を指定してキャラクターを移動させることができます
 
-出来上がったら `npm publish` してください。以上です。
+足踏みをさせるには、`g.FrameSprite` 同様 `start` を呼び出してください。以下のように使います。
+
+```typescript
+const chara4 = new entities.Character4({
+	scene: this,
+	src: this.asset.getImage("/assets/characters/pipo-charachip001.png"),
+	animationFrameCount: 3,
+	interval: 200,
+	width: 32,
+	height: 32,
+	parent: this,
+	x: g.game.width / 2,
+	y: g.game.height / 2,
+});
+chara4.start();
+this.onPointDownCapture.add((e) => {
+	chara4.animationMoveTo(
+		Math.floor(e.point.x / 32) * 32,
+		Math.floor(e.point.y / 32) * 32,
+	);
+});
+```
 
 ## LICENSE
 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,127 @@
-export class Hoge extends g.E {
+export type Character4Direction = "left" | "right" | "top" | "down";
+
+export interface Character4ParameterObject extends g.FrameSpriteParameterObject {
+	animationFrameCount: number;
+	initialAngle?: Character4Direction;
+}
+
+const angleFrameIndex = {
+	left: 1,
+	down: 0,
+	top: 3,
+	right: 2,
+};
+
+export class Character4 extends g.FrameSprite {
+	destinationPos?: g.CommonOffset & { time: number };
+
+	animationFrameCount: number;
+
+	animationInterval: number;
+
+	currentDirection: Character4Direction;
+
+	moveSpeed: number;
+
+	constructor(params: Character4ParameterObject) {
+		super(params);
+		this.animationFrameCount = params.animationFrameCount;
+		this.currentDirection = params.initialAngle ?? "down";
+		this.frames = [];
+		this.destinationPos = null;
+		for (let i = 0; i < this.animationFrameCount; i++) {
+			this.frames.push(0);
+		}
+		this._changeFramesByDirection();
+		this.onUpdate.add(this._handleCharacterUpdate, this);
+		this.moveSpeed = 0.003 * 1000 / g.game.fps;
+	}
+
+	_changeFramesByDirection() {
+		const offset = this.animationFrameCount * angleFrameIndex[this.currentDirection];
+		this.frames = this.frames.map((_, index) => offset + index);
+		this.modified();
+	}
+
+	_changeAngleByDestination() {
+		const xDiff = this.destinationPos.x - this.x;
+		const yDiff = this.destinationPos.y - this.y;
+		if (Math.abs(xDiff) < Math.abs(yDiff)) {
+			if (yDiff < 0) {
+				this.changeDirection("top");
+			} else {
+				this.changeDirection("down");
+			}
+		} else if (Math.abs(xDiff) > Math.abs(yDiff)) {
+			if (xDiff < 0) {
+				this.changeDirection("left");
+			} else {
+				this.changeDirection("right");
+			}
+		}
+	}
+
+	_handleCharacterUpdate() {
+		if (this.destinationPos != null) {
+			let diff = 1000 / g.game.fps;
+			if (diff > this.destinationPos.time) {
+				diff = this.destinationPos.time;
+			}
+			const x = (this.destinationPos.x - this.x) * (diff / this.destinationPos.time);
+			const y = (this.destinationPos.y - this.y) * (diff / this.destinationPos.time);
+			this.destinationPos.time -= diff;
+			if (this.destinationPos.time === 0) {
+				this.moveTo(this.destinationPos.x, this.destinationPos.y);
+				this.destinationPos = null;
+			} else {
+				this.moveBy(x, y);
+			}
+			this.modified();
+		}
+	}
+
+	changeDirection(direction: Character4Direction) {
+		if (this.currentDirection === direction) return;
+		this.currentDirection = direction;
+		this._changeFramesByDirection();
+	}
+
+	move(angle: Character4Direction, distance: number) {
+		switch (angle) {
+		case "left":
+			this.changeDirection(angle);
+			this.x -= distance;
+			break;
+		case "right":
+			this.changeDirection(angle);
+			this.x += distance;
+			break;
+		case "top":
+			this.changeDirection(angle);
+			this.y -= distance;
+			break;
+		case "down":
+			this.changeDirection(angle);
+			this.y += distance;
+			break;
+		default:
+			throw new Error(`Invalid angle: ${angle}`);
+		}
+	}
+
+	animationMoveTo(x: number, y: number, time?: number) {
+		this.destinationPos = {x, y,
+			time: time == null ? Math.max(Math.abs(x - this.x), Math.abs(y - this.y)) / this.moveSpeed : time,
+		};
+		this._changeAngleByDestination();
+	}
+
+	animationMoveBy(x: number, y: number, time?: number) {
+		this.destinationPos = {
+			x: this.x + x,
+			y: this.y + y,
+			time,
+		};
+		this._changeAngleByDestination();
+	}
 }
